0a1f0e5a30f8c3f0761058cf852cd73c4ee58a2e
Lorenzo Fontana <gufoscuro@gmail.com>
2023-02-07 11:43:11 +0100

    templates

--- src/lib/utils/templates.ts
+++ src/lib/utils/templates.ts
@@ -7,8 +7,8 @@ export function templateReplacer (templates: TemplateMap, name: string, content:
 }
 
 export function templateMatcher (templates: TemplateMap, content: string): string {
-    return content.replace(/{{!?template name\(([a-zA-Z\-]+)\)( content\([a-zA-Z\-\s:]+\))?!?}}/g, function($0, $1, $2) {
-        const matches = $2 ? $2.match(/content\(([a-zA-Z\-\s:]+)\)/) : null;
+    return content.replace(/{{!?template name\(([a-zA-Z\-]+)\)( content\([a-zA-Z0-9!@#\$%\^\&*\-\s:,;\.]+\))?!?}}/g, function($0, $1, $2) {
+        const matches = $2 ? $2.match(/content\(([a-zA-Z0-9!@#\$%\^\&*\-\s:,;\.]+)\)/) : null;
         const templateContent = matches ? matches[1] : '';
         // console.log ('templateMatcher', $1, $2, templateContent)
         return templateReplacer(templates, $1, templateContent)
